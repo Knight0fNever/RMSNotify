fd1b4c56ed2deb3ef8dd8a2cfa539e4e2fca4086
Josh <jconner050612@gmail.com>
2017-01-31 11:44:21 -0800

    Added Code for Orders

--- src/com/zc/Filer.java
+++ src/com/zc/Filer.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Filer {
-    public static void write(int transNumber) {
+    public static void writeTrans(int transNumber) {
         try {
             File fac = new File(".\\JavaNotifier.txt");
             if (!fac.exists()) {
@@ -24,10 +24,37 @@ public class Filer {
             e.printStackTrace();
         }
     }
-    public static int read() {
+    public static int readTrans() {
         int result = 0;
         try {
             Scanner scanner = new Scanner(new File(".\\JavaNotifier.txt"));
+            result = scanner.nextInt();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public static void writeOrder(int orderNumber) {
+        try {
+            File fac = new File(".\\JavaOrderNotifier.txt");
+            if (!fac.exists()) {
+                fac.createNewFile();
+            }
+            int r = 1;
+            FileWriter write = new FileWriter(fac);
+            write.write(new Integer(orderNumber).toString());
+            write.flush();
+            write.close();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int readOrder() {
+        int result = 0;
+        try {
+            Scanner scanner = new Scanner(new File(".\\JavaOrderNotifier.txt"));
             result = scanner.nextInt();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
